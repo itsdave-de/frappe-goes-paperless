6606a678f8d95903fecabea0bfd62c1b62c3f408
Frappe Devel <admin@itsdave.de>
2024-08-15 15:41:02 +0200

    sync changes on gui

---
+++ frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document_type_mapping/paperless_document_type_mapping.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, itsdave GmbH and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Paperless Document Type Mapping", {
+// 	refresh(frm) {
+
+// 	},
+// });
