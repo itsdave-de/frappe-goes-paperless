8ecf927860331466d34b1be22c69fd17b086b6bf
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-08-15 17:29:55 -0300

    remove incorrect syntax

--- frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.py
+++ frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.py
@@ -117,7 +117,7 @@ def get_ai_data(self):
 		self.ai_response_json = 'The content is not in JSON format'
 	self.status = 'AI-Response-Recieved'
 	self.save()
-	frappe.show_alert('Response received successfully, fields updated!')
+	##frappe.msgprint('Response received successfully, fields updated!')
 
 
 @frappe.whitelist()
