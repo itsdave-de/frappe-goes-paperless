9e61cbdc20e4db285d8784a8a7ee4cdc2aa32aec
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-08-15 11:11:53 -0300

    add click over js

--- frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.js
+++ frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, itsdave GmbH and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Paperless Document", {
-// 	refresh(frm) {
+frappe.ui.form.on("Paperless Document", {
+    onload: function(frm) {
+        frm.fields_dict['get_ai_data'].df.on_click = function() {
+            frappe.show_alert('Starting query in AI...');
+            frappe.call({
+                method: 'button_get_ai'
+            });
+        };
+    }
+});
 
-// 	},
-// });
