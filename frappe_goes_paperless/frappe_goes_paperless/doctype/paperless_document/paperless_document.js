02451c3d8afd6619bcef396a328cb4a371d18b99
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-08-16 07:01:36 -0300

    try fix js buttons

--- frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.js
+++ frappe_goes_paperless/frappe_goes_paperless/doctype/paperless_document/paperless_document.js
@@ -3,25 +3,23 @@
 
 frappe.ui.form.on("Paperless Document", {
     refresh: function(frm) {
-        frm.add_custom_button(__('Actions'), (frm) => {
-            frm.page.add_action_item(__('Get AI data'), () => {
-                frappe.show_alert('Starting query in AI...');
-                frappe.call({
-                    method: 'frappe_goes_paperless.frappe_goes_paperless.doctype.paperless_document.paperless_document.button_get_ai',
-                    args: { doc: frm.doc },
-                    callback: function(response) {
-                        if (response.message) {
-                            const jobId = response.message;
-                            console.log('jobid -> ' + jobId);
-                            verificarStatusJob(jobId);
-                        }
+        frm.add_custom_button(__('Get AI data'), (frm) => {
+            frappe.show_alert('Starting query in AI...');
+            frappe.call({
+                method: 'frappe_goes_paperless.frappe_goes_paperless.doctype.paperless_document.paperless_document.button_get_ai',
+                args: { doc: frm.doc },
+                callback: function(response) {
+                    if (response.message) {
+                        const jobId = response.message;
+                        console.log('jobid -> ' + jobId);
+                        verificarStatusJob(jobId);
                     }
-                });
+                }
             });
-            frm.page.add_action_item(__('Open document on Paperless'), (frm) => {
-                window.open('http://10.251.0.55:8000/documents/' + frm.doc.paperless_document_id + '/details', '_blank');
-            });
-        });
+        }, __("Actions"));
+        frm.add_custom_button(__('Open document on Paperless'), (frm) => {
+            window.open('http://10.251.0.55:8000/documents/' + frm.doc.paperless_document_id + '/details', '_blank');
+        }, __("Actions"));
     }
 });
 
