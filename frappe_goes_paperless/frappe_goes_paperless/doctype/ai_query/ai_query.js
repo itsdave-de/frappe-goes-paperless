4733686175eb7c903a0327854102373a9a3ff50c
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-08-26 08:14:01 -0300

    refresh field

--- frappe_goes_paperless/frappe_goes_paperless/doctype/ai_query/ai_query.js
+++ frappe_goes_paperless/frappe_goes_paperless/doctype/ai_query/ai_query.js
@@ -18,6 +18,7 @@ frappe.ui.form.on("AI Query", {
                     if (r.message) {
                         // Display the response as a message
                         frappe.msgprint(r.message);
+                        frappe.refresh_field('supplier');
                     } else {
                         frappe.msgprint(__('No response received.'));
                     }
