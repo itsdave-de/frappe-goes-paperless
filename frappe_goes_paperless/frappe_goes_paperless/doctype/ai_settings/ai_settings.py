4fcd078ea81222686067c10c7fb3c19c7d30de0c
Luiz Gustavo Costa <luizgustavo@luizgustavo.pro.br>
2024-09-23 08:49:48 -0300

    Remove unused AI-related files

--- frappe_goes_paperless/frappe_goes_paperless/doctype/ai_settings/ai_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, itsdave GmbH and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class AISettings(Document):
-	pass
